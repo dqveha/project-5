f4ea1256f23eea4e00015559b01cfdfccc113be5
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-06-25 08:44:40 -0700

    Create GalacticAges constructor with DOB and planet properties; jest pass

---
+++ __tests__/galactic-calc.test.js
@@ -0,0 +1,23 @@
+import GalacticAges from '../src/js/galactic-calc.js';
+
+describe('GalacticAges', () => {
+  let ages;
+
+  beforeEach(() => {
+    ages = new GalacticAges(1990, 9, 3);
+  })
+
+  test('should properly create an age object with properties defining years in Earth, Mercury, Venus, Mars, and Jupiter and another property to input date of birth', () => {
+    
+    expect(ages.year).toEqual(1990);
+    expect(ages.month).toEqual(9);
+    expect(ages.day).toEqual(3);
+    expect(ages.earthAge).toEqual("");
+    expect(ages.mercuryAge).toEqual("");
+    expect(ages.venusAge).toEqual("");
+    expect(ages.marsAge).toEqual("");
+  });
+});
+  
+  
+  
---
+++ src/js/galactic-calc.js
@@ -0,0 +1,11 @@
+export default class GalacticAges {
+  constructor(year, month, day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+    this.earthAge = "";
+    this.mercuryAge = "";
+    this.venusAge = "";
+    this.marsAge = "";
+  }
+}
